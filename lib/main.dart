5a0778dfbb5432c51ef7ae755441634349c973e7
YukiHayashi <yuu.tennis.10.21.6@gmail.com>
2022-01-02 21:42:45 +0900

    step2完了

--- lib/main.dart
+++ lib/main.dart
@@ -27,10 +27,27 @@ class MyHomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.network(
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-              height: 100,
-              width: 100,
+            Stack(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(32),
+                  child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: const Text(
+                    'No.25',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             ),
             const Text(
               'pikachu',
@@ -41,7 +58,15 @@ class MyHomePage extends StatelessWidget {
             //   label: Text('electric'),
             //   backgroundColor: Colors.yellow,
             // ),
-            
+              // Containerでの実装
+            Container(
+              child: const Text('electric'),
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
         ]),
       ),
     );
